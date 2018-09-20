d2e3f02b2d2df20fee1923819d2d9ddd828d306d
Saul Galaviz <nostramagear@gmail.com>
2018-09-19 18:34:08 -0700

    New Solution

---
+++ ReverseInteger.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reverse(int x) {
+        if (x > Integer.MAX_VALUE || x < Integer.Min_Value)
+            return 0;
+        
+        boolean negative = false;
+        if (x < 0)
+            negative = true;
+        
+        
+        
+        
+    }
+}
